2f334c784a4e144b63df67c65e4a2670df505534
Simon Kleber <kleber.simon@t-online.de>
2024-05-19 12:04:42 +0200

    Change fontend linting directory

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -1,23 +1,21 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
-  testEnvironment: "jsdom",
-  roots: [
-    '<rootDir>/src',
-    '<rootDir>/test'
-  ],
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/test'],
   moduleNameMapper: {
+    // eslint-disable-next-line max-len
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/src/__mocks__/fileMock.ts',
-    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.ts',
+      '<rootDir>/src/__mocks__/fileMock.ts',
+    '\\.(css|less)$': '<rootDir>/src/__mocks__/styleMock.ts'
   },
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   coveragePathIgnorePatterns: [
     '/src/index.tsx',
     '/src/@types/',
-    '/src/__mocks__/',
-  ],
-};
+    '/src/__mocks__/'
+  ]
+}
 
 export default config
