abc106a7e5957ae59ad4c9fb056f4b3b6d1da72f
Simon Kleber <kleber.simon@t-online.de>
2024-05-22 01:22:53 +0200

    Fix frontend test

--- frontend/test/App.test.tsx
+++ frontend/test/App.test.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import App from '../src/App'
 import '@testing-library/jest-dom'
 
-test('renders learn react link', () => {
+test('renders app', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
 })
